5227f5f4d1cff97c2cdb3ecb0fa8f73f151a70df
Albert Danilov <albert95danilov@gmail.com>
2022-06-06 14:12:25 +0300

    commit code

---
+++ TestAPIwithSwagger/Controllers/AuthenticationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TestAPIwithSwagger.Controllers
+{
+    public class AuthenticationController : ApiController
+    {
+        [HttpPost]
+        [Route("Authentication")]
+        public JObject authenticationService([FromBody] JObject authenticationJson)
+        {
+            JObject retJson = new JObject();
+            string username = authenticationJson["username"].ToString();
+            string password = authenticationJson["password"].ToString();
+            if (username == "user" && password == "user")
+            {
+                retJson.Add(new JProperty("authentication ", "successful"));
+            }
+            else
+            {
+                retJson.Add(new JProperty("authentication ", "unsuccessful"));
+            }
+            return retJson;
+        }
+    }
+}
